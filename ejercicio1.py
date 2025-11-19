d28ee44cc6d4c628ee739480180d06e4bf4d984d
al06040465 <al06040465@udelg.unigolfo.edu.mx>
2025-11-19 09:06:43 -0600

    Add user data input function in ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+def ejercicio1():
+    print("\n--- EJERCICIO1: datos del usuario ---")
+    nombre = input("nombre: ")
+    edad = input("edad: ")
+    carrera del usuario = input("carrera: ")
+    printf("\n Hola{nombre},edad{édad},carrera del usuario{carrera})
