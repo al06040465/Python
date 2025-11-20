0d902f84777b9086514c1a9f827722cce716074c
al06040465 <al06040465@udelg.unigolfo.edu.mx>
2025-11-20 13:44:14 -0600

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,23 +1,19 @@
 def ejercicio3():
-    print("\n--- EJERCICIO 3: Suma y promedio ---")
-    try:
-        n = int(input("¿Cuántos números vas a ingresar?: "))
-    except:
-        print("Entrada inválida.")
-        return
-
-    suma = 0
-
+   print("\n---- EJERCICIO3: suma y promedio")
+         try:
+             n=int(input("cuantos numeros vas a ingresar?: ")
+        except:
+               print("entrada invalida.")
+              return
+       suma=0
     for i in range(n):
         try:
-            num = float(input(f"Número {i+1}: "))
+            num = float(input(f"numero{i+1}: "))
             suma += num
         except:
-            print("Valor inválido. Se toma como 0.")
-            continue
-
-    promedio = suma / n if n > 0 else 0
-
-    print("\nCantidad ingresada:", n)
-    print("Suma total:", suma)
-    print("Promedio:", promedio)
+               print("valor invalido. se tomacomo 0.")
+               continue
+        promedio = suma / n if n > 0 else 0
+        print("\ncantidad ingresada:", n)
+        print("suma total:",suma)
+        print("primedio:",promedio)
