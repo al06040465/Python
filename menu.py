30b3b915f941f6b29d529c542f6435b0d57a3042
al06040465 <al06040465@udelg.unigolfo.edu.mx>
2025-11-21 09:29:48 -0600

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,21 @@
+def mostrar_menu():
+  while true:
+      print("\n===== MENU PRINCIPAL =====")
+      PRINT("1. DATOS DEL USUARO")
+      print("0. para salir")
+def ejercicio1():
+  print("ejercicio 1 datos del usuario")
+  nombre = input("nombre: ")
+  edad = input("edad: ")
+  carrera = input("carrera: ")
+def ejercicio():
+  print("ejercicio2 condicionales")
+  try:
+      num = float(input("ingresa un numero")
+      print("entrada invalida")
+  except:
+        print("entrada invalida")
+def ejercicio3():
+  print(ejercicio#)
+     
+  
