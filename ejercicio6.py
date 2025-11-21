6409fdc8b1420d47da30fad5e8e88b7e1aca1e36
al06040465 <al06040465@udelg.unigolfo.edu.mx>
2025-11-21 09:10:29 -0600

    Implement ejercicio6 function with student dictionary

---
+++ ejercicio6.py
@@ -0,0 +1,10 @@
+def ejercicio6():
+  print("\n--- EJERCICIO6: diccionario alumno ---")
+  diccionario_alumnos = {
+        "101":{nombre: matricula: carrera: promedio:}
+  if:
+    alumno["promedio"]>=7
+    print("estado: aprobado")
+    else:
+         alumno["promedio"]<=7
+          print("estado: reprobado")
